9eb3ddfccb09d0d610e531c21495456924b2107b
Fetonte1960 <diegoquattrone2@hotmail.com>
2026-09-21 11:54:35 +0200

    Fix xBIM unit enum namespace

--- Server/Termodelwebservice/src/Termodel.Core/CopiedFromTermodel/Model/Modello.cs
+++ Server/Termodelwebservice/src/Termodel.Core/CopiedFromTermodel/Model/Modello.cs
@@ -7,6 +7,7 @@ using Xbim.Ifc;
 using Xbim.Ifc4.GeometryResource;
 using Xbim.Ifc4.GeometricConstraintResource;
 using Xbim.Ifc4.Kernel;
+using Xbim.Ifc4.Interfaces;
 using Xbim.Ifc4.MeasureResource;
 using Xbim.Ifc4.ProductExtension;
 using Xbim.Ifc4.RepresentationResource;
